5415caf9a3ec613fa18dc4b40cd40a5ec9966779
gauravideshpande06 <deshpandegauravi26@gmail.com>
2022-10-27 14:24:47 +0530

    Key added to Job Posting

--- SRecruitAPI/Models/SpJobPosting.cs
+++ SRecruitAPI/Models/SpJobPosting.cs
@@ -5,13 +5,14 @@ namespace SRecruitAPI.Models
     public class SpJobPosting
     {
         [Key]
+        public Int64 id { get; set; }
         public int job_posting_id { get; set; }
         public string Company_name { get; set; }
         public string Location { get; set; }
-
         public int job_posting_yoe { get; set; }
         public string job_role_title { get; set; }
         public string job_skills_title { get; set; }
+        public int job_skills_id { get; set; }
     }
 }
 
